9e2da509d50a00700ddb96c1ca7295475a429bd7
denis <denis@debian>
2020-07-09 01:39:20 +0200

    debugging

--- backend-websocket/src/server.js
+++ backend-websocket/src/server.js
@@ -219,11 +219,11 @@ const initEngineDevices = (io) => {
 
             console.log("setInit: ", object);
             console.log("socket id: ", socket.id);
-            const results = await mysqlQuery('SELECT * FROM device WHERE mac_address = ?', [object.mac]);
+            let results = await mysqlQuery('SELECT * FROM device WHERE mac_address = ?', [object.mac]);
             console.log("results: ", results.length);
             if (results.length == 0) {
                 console.log("results after: ", results);
-                const results = await mysqlQuery(`INSERT INTO device(room_id, mac_address, socket_id, state) VALUES(NULL, "${object.mac}", "${socket.id}", "${object.state}"`);
+                results = await mysqlQuery(`INSERT INTO device(room_id, mac_address, socket_id, state) VALUES(NULL, "${object.mac}", "${socket.id}", "${object.state}"`);
                 console.log("results after: ", results);
             } else {
                 const results = await mysqlQuery(`UPDATE device SET socket_id = "${socket.id}", state = ${object.state} WHERE mac_address = "${object.mac}"`);
